5e9057281a3b6112b0d860b7a6113746e6b0daa4
Dredex <root.doda@gmail.com>
2024-04-24 18:06:21 +0200

    Added dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  let [dropDown, setDropDown] = useState("levelDropDownHidden");
+
+  const dropClickEvent = () => {
+    dropDown == "levelDropDownHidden"
+      ? setDropDown("levelDropDownShow")
+      : setDropDown("levelDropDownHidden");
+  };
+
   return (
     <div className="mainContent">
       <div className="navBar">
-        <div className="levels">
+        <div className="levels" onClick={dropClickEvent}>
           <p>Level 0</p>
+          <div className={"levelDropDown " + dropDown}>
+            <p>Level 1</p>
+            <p>Level 2</p>
+            <p>Level 3</p>
+            <p>Level 4</p>
+            <p>Level 5</p>
+            <p>Level 6</p>
+            <p>Level 7</p>
+            <p>Level 8</p>
+          </div>
         </div>
         <div className="heading">
           <a>TypeMaster.com</a>
